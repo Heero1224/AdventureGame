b3bfbad2f0ea587b2c3e26fbae746d0613fc75bf
Joshua S. Andrews <jsandrews@email.arizona.edu>
2022-03-31 19:10:19 -0700

    Race abstract made

---
+++ src/model/Item.java
@@ -0,0 +1,29 @@
+package model;
+
+/**
+ * @author Heero
+ *
+ */
+public class Item {
+    private String name;
+    private int	ac;
+    private String[] special;
+    
+    public Item(String name, int ac, String[] special) {
+	this.name = name;
+	this.ac = ac;
+	this.special = special;
+    }
+    
+    public String getName() {
+	return name;
+    }
+    
+    public String[] getSpecial() {
+	return special;
+    }
+    
+    public int getAC() {
+	return ac;
+    }
+}
---
+++ src/model/Race.java
@@ -0,0 +1,102 @@
+package model;
+
+import java.util.List;
+
+/**
+ * Purpose:
+ * 	Abstract stat block chart for a creature
+ * @author Joshua S. Andrews
+ *
+ */
+public abstract class Race {
+    private String name;
+    private String desc; // Description of a race
+    private int[] bonus;
+    private String size; 
+    private int speed;
+    private String[] languages;
+    
+    public Race(String name, String desc, int[] bonus, String size, int speed, String[] languages) {
+	this.setName(name);
+	this.desc = desc;
+	this.bonus = bonus;
+	this.setSize(size);
+	this.setSpeed(speed);
+	this.setLanguages(languages);
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+	return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+	this.name = name;
+    }
+    
+    public String getDesc() {
+	return desc;
+    }
+
+    /**
+     * @return the bonus
+     */
+    public int[] getBonus() {
+	return bonus;
+    }
+
+    /**
+     * @param bonus the bonus to set
+     */
+    public void setBonus(int[] bonus) {
+	this.bonus = bonus;
+    }
+
+    /**
+     * @return the size
+     */
+    public String getSize() {
+	return size;
+    }
+
+    /**
+     * @param size the size to set
+     */
+    public void setSize(String size) {
+	this.size = size;
+    }
+
+    /**
+     * @return the speed
+     */
+    public int getSpeed() {
+	return speed;
+    }
+
+    /**
+     * @param speed the speed to set
+     */
+    public void setSpeed(int speed) {
+	this.speed = speed;
+    }
+
+    /**
+     * @return the languages
+     */
+    public String[] getLanguages() {
+	return languages;
+    }
+
+    /**
+     * @param languages the languages to set
+     */
+    public void setLanguages(String[] languages) {
+	this.languages = languages;
+    }
+}
+
